76615f152ebfc0a0ee35064e362fff8bf8c1d7d3
Nicholas Dorland <11059821@uvu.edu>
2024-09-09 23:21:54 -0600

    Matching Game Part 1
    Complete Lab assignment. Had to restart the assignment as I began it in a 3D game, and being a 2D game was important for this assignment.

---
+++ Matching_Game_Part_1/Assets/Scripts/FloatData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+/*************************************************************************
+ * This creates Scriptable Objects that hold a Float value between scenes, 
+ * good for health/scores.
+ ************************************************************************/
+
+[CreateAssetMenu]
+
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void SetValue(float num)
+    {
+        value = num;
+    }
+
+    // Modify the value when an attached boject is interacted with.
+    public void UpdateValue(float num)
+    {
+        value += num;
+    }
+}
