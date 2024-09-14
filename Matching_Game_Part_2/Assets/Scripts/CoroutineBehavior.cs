960c3155efea274370cd294a01a88ff8c32de4a3
Nicholas Dorland <11059821@uvu.edu>
2024-09-14 17:51:34 -0600

    MG2 Lab Update
    Followed along and completed the Coroutines Lesson.

--- Matching_Game_Part_2/Assets/Scripts/CoroutineBehavior.cs
+++ Matching_Game_Part_2/Assets/Scripts/CoroutineBehavior.cs
@@ -1,12 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoroutineBehavior : MonoBehaviour
 {
-    IEnumerator Start()
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+
+    public bool canRun;
+    public IntData counterNum;
+    public float seconds = 3.0f;
+
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    public void Start()
     {
-        yield return new WaitForSeconds(3);
-        Debug.Log("Late Start");
+        startEvent.Invoke();
+    }
+
+    public void startCounting()
+    {
+        StartCoroutine(Counting());
+    }
+
+    private IEnumerator Counting()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        startCountEvent.Invoke();
+        yield return wfsObj;
+
+        while (counterNum.value > 0)
+        {
+            repeatCountEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }
+
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
     }
 }
